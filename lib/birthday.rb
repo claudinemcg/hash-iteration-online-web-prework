c171deabb463f7fd5b405e227a594a6489cb9bbd
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-09 21:21:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,10 +6,14 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end
+end
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
     if age <= 12
       puts "Happy Birthday #{name}! You are now #{age} years old!"
     end
   end
 end
-
-
